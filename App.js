0f0949b5f8619ec29ff9e140694b7bcd895841b8
KastroI79 <90890835+KastroI79@users.noreply.github.com>
2025-02-10 18:00:56 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Remont D'Castro</h1>
+      <p>Това е уеб приложение за 3D визуализации.</p>
+    </div>
+  );
+}
+
+export default App;
